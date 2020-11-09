69a027fb2060e080dd5beb3104c132f21f0f77f5
Felix Kuhn <felix.kuhn@hs-furtwangen.de>
2020-11-09 13:15:00 +0100

    JavaDoc added

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,16 +1,22 @@
 package de.hfu;
 import java.io.*;
 /**
- * Hello world!
- *
+ * 
+ * @author Felix K.
+ * @version 1.0
  */
 public class App 
 {
+	/**
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
     public static void main( String[] args ) throws IOException
     {
         InputStreamReader isr = new InputStreamReader(System.in);
     	BufferedReader br = new BufferedReader(isr);
-	String input = br.readLine();
-	System.out.println(input.toUpperCase());
+    	String input = br.readLine();
+    	System.out.println(input.toUpperCase());
     }
 }
